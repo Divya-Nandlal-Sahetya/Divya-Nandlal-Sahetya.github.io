b3f2ac8ede682de5539f931b6b6daa1993eced89
Divya Nandlal Sahetya <sahetya@usc.edu>
2023-06-02 02:19:37 -0700

    fixes

--- app.js
+++ app.js
@@ -11,3 +11,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+function filterSection(current, tagsToShow){
+    [...document.getElementsByClassName("portfolio-header")].forEach(element => element.classList="portfolio-header portfolio-title")
+    current.classList = "portfolio-header portfolio-title portfolio-header-active"
+    classes = ['section-ml', 'section-dl', 'section-sde']
+    for (const cl of classes) {
+        [...document.getElementsByClassName(cl)].forEach(element => element.style.display = 'none')
+    }
+    for (const cl of tagsToShow) {
+        [...document.getElementsByClassName(cl)].forEach(element => element.style.display = 'block')
+    }
+}
